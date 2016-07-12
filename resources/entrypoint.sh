9d8fb6cfa29e67c463b812a031a99b38e03d8531
Steve Mostovoy <stevemostovoysm@gmail.com>
2016-07-12 10:38:54 -0700

    Change favicon.

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -2,13 +2,13 @@
 
 /usr/sbin/nginx -c /app/resources/nginx.conf -p /app/
 cd /app
-(
-	cd frontend
-	MINIFY=1 ./node_modules/webpack/bin/webpack.js --progress
-)
 
 if [[ "$MODE" == "dev" ]]; then
 	./node_modules/nodemon/bin/nodemon.js src/index.js
 elif [[ "$MODE" == "release" ]]; then
+	(
+		cd frontend
+		MINIFY=1 ./node_modules/webpack/bin/webpack.js --progress
+	)
 	nodejs src/index.js
 fi
